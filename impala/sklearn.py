7a6b2060ecd884d22a9e6dc01ecbf07aea5566f1
Uri Laserson <laserson@cloudera.com>
2013-12-12 17:35:28 -0800

    Some random sketches for sklearn

--- impala/sklearn.py
+++ impala/sklearn.py
@@ -1,33 +1,105 @@
 from sklearn.base import BaseEstimator
 
+import impala.util
+
 
 class DataSet(object):
+    """Represents a (labeled) data set backed by Impala.
     
-    def __init__(self, cursor, query_string, label=None):
+    The underlying data could be a persisted table, or an unmaterialized query
+    string.
+    
+    Parameters
+    ----------
+    cursor
+    query_string
+    table_name
+    """
+    
+    def __init__(self, cursor, **kw):
         self.cursor = cursor
-        self.query_string = query_string
-
-
-
+        
+        if 'table_name' in kw:
+            if 'query_string' in kw:
+                raise ValueError("Exactly one of `table_name` and `query_string` "
+                                 "must be provided.")
+            self.table_name = kw['table_name']
+            self.query_string = 'SELECT * FROM %s' % self.table_name
+            self.is_persisted = True
+        elif 'query_string' in kw:
+            self.table_name = None
+            self.query_string = kw['query_string']
+            self.is_persisted = False
+        else:
+            raise ValueError("Exactly one of `table_name` and `query_string` "
+                             "must be provided.")
+        
+        self.schema = util.compute_result_schema(self.cursor, self.query_string)
 
 
 
 class ImpalaLogisticRegression(BaseEstimator):
     
-    def __init__(self, step_size=0.1, mu=0.1):
+    def __init__(self, step_size=0.1, mu=0.1, n_iter=5):
         self.step_size = step_size
         self.mu = mu
+        self.n_iter = n_iter
         self.coef_ = None
         return self
     
-    def partial_fit(self, cursor, query_string, label_column):
-        schema = util.compute_result_schema(cursor, query_string)
+    def partial_fit(self, cursor, blobstore,  query_string, label_column, epoch, ):
+        
+        blobstore.put(epoch + 1, )
+        
+        selectable = """
+                %(udf_name)s(%(model)s, %(param)s, %(observation)s)
+                """ % ('logr', 'model_table.value', 'param_table.value', 'toarray(data_table.*)')
+        
+        logistic_regression_query = """
+                INSERT INTO %(blob_store)s
+                SELECT %(key)i, %(selectable)s
+                FROM %(table)s
+                """ % ('model_table', new_epoch, selectable, cross_prod_sql_gen)
+        
+        
+        
+        
+        logistic_regression_query = """
+            INSERT INTO %(model_table)s
+            SELECT %(epoch)i, logr(%(model_table)s.model, toarray(), %(label_column)s, %(step_size)f, %(mu)f)
+            FROM %(model_table)s INNER JOIN %(data_table)s
+            ON (%(model_table)s.model is null || true) = (%(data_table)s.%(hack)s is null || true)
+            WHERE %(model_table)s.iter = %(prev_epoch)i
+            """
+            
+            
+        
+        
+        schema = impala.util.compute_result_schema(cursor, query_string)
         
         if self.coef_ is None:
             self.coef_ = np.zeros()
     
-    def fit(self, cursor, query_string, label_column):
-        if self.coef_ is None
+    def fit(self, cursor, query_string, label_column, model_table=None):
+        self.coef_ = np.zeros()
+        model_table = impala.util.make_model_table(cursor, model_table=model_table)
+        for i in xrange(self.n_iter):
+            self.partial_fit(cursor, query_string, label_column, i+1, model_table)
+        
+        
+        """
+        INSERT INTO %(model_table)s
+        SELECT %(epoch)i, encodearray(logr(decodearray(model_table.model), ))
+        
+    FROM %(model_table)s
+    INNER JOIN %(data_table)s
+    ON (%(model_table)s.model is null || true) = (%(data_table)s.%(hack)s is null || true)
+    WHERE %(model_table)s.iter = %(epoch)i
+    """
+        
+        
+        if self.coef_ is None:
+            pass
         "SELECT logr(%(model_state)s,"
                        "%(obs_as_str)s,"
                        "%(label_column)s,
