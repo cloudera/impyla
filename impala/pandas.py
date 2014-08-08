ed73db7b79889c4c828f392a6484b0a73cbcffa4
Uri Laserson <laserson@cloudera.com>
2014-08-07 18:19:56 -0700

    Mostly work on creating BDFs

--- impala/pandas.py
+++ impala/pandas.py
@@ -14,13 +14,33 @@
 
 import string
 import random
+import datetime
 from copy import copy
+from cStringIO import StringIO
+
+import pandas as pd
 
 from .util import as_pandas
 
 # some rando utilities
 
 _null_slice = slice(None, None, None)
+
+# Here we map the TTypeId types from TCLIService (which are returned for schema
+# results by the RPC/DBAPI tools) back to the Impala-recognized types (i.e.,
+# com.cloudera.impala.catalog.PrimitiveType); this is the inverse of the
+_TTypeId_to_PrimitiveType = {
+	'BOOLEAN_TYPE': 'BOOLEAN',
+	'TINYINT_TYPE': 'TINYINT',
+	'SMALLINT_TYPE': 'SMALLINT',
+	'INT_TYPE': 'INT',
+	'BIGINT_TYPE': 'BIGINT',
+	'TIMESTAMP_TYPE': 'TIMESTAMP',
+	'FLOAT_TYPE': 'FLOAT',
+	'DOUBLE_TYPE': 'DOUBLE',
+	'STRING_TYPE': 'STRING',
+	'DECIMAL_TYPE': 'DECIMAL',
+}
 
 def _random_id(prefix='', length=8):
     return prefix + ''.join(random.sample(string.ascii_uppercase, length))
@@ -31,6 +51,41 @@ def _get_schema_hack(cursor, table_ref):
     schema = [tup[:2] for tup in cursor.description]
     cursor.fetchall() # resets the state of the cursor and closes operation
     return schema
+
+def _to_TableName(table):
+    if not isinstance(table, basestring):
+	raise ValueError("table must be a string")
+    if table == '':
+	raise ValueError("table must not be the empty string")
+    fields = table.split('.')
+    if len(fields) == 2:
+	db = fields[0]
+	name = fields[1]
+    elif len(fields) == 1:
+	db = None
+	name = fields[0]
+    else:
+	raise ValueError("your value for table (%s) is weird" % table)
+    return TableName(name, db)
+
+def _numpy_dtype_to_impala_PrimitiveType(ty):
+    # based on impl in pandas.io.sql.PandasSQLTable._sqlalchemy_type()
+    if ty is datetime.date:
+	# TODO: this might be wrong
+	return 'TIMESTAMP'
+    if pd.core.common.is_datetime64_dtype(ty):
+	# TODO: this might be wrong
+	return 'TIMESTAMP'
+    if pd.core.common.is_timedelta64_dtype(ty):
+	return 'BIGINT'
+    elif pd.core.common.is_float_dtype(ty):
+	return 'DOUBLE'
+    elif pd.core.common.is_integer_dtype(ty):
+	# TODO: BIGINT may be excessive?
+	return 'BIGINT'
+    elif pd.core.common.is_bool(ty):
+	return 'BOOLEAN'
+    return 'STRING'
 
 
 # PUBLIC API
@@ -45,44 +100,74 @@ def read_sql_query(cursor, query, alias=None):
 
 def read_sql_table(cursor, table):
     """Create a BDF from a table name usable in Impala"""
-    if not isinstance(table, basestring):
-	raise ValueError("table must be a string")
-    if table == '':
-	raise ValueError("table must not be the empty string")
-    fields = table.split('.')
-    if len(fields) == 2:
-	db = fields[0]
-	name = fields[1]
-    elif len(fields) == 1:
-	db = None
-	name = fields[0]
-    else:
-	raise ValueError("your value for table is weird")
-    table_name = TableName(name, db)
+    table_name = _to_TableName(table)
     table_ref = BaseTableRef(table_name)
     schema = _get_schema_hack(cursor, table_ref)
     select_list = tuple([SelectItem(expr=Literal(col)) for (col, ty) in schema])
     return BigDataFrame(SelectStmt(select_list, table_ref))
 
-def read_hdfs(cursor, path, schema):
-    """Create a BDF from an Impala-compatible file in HDFS
+def register_external_table(cursor, table, table_schema, path,
+	file_format='TEXTFILE', partition_schema=None, field_terminator='\\t',
+	line_terminator='\\n'):
+    table_name = _to_TableName(table)
+    query = "CREATE EXTERNAL TABLE %s" % table_name.to_sql()
+    schema_string = ', '.join(['%s %s' % (col, _TTypeId_to_PrimitiveType[ty])
+	    for (col, ty) in table_schema])
+    query += " (%s)" % schema_string
+    if partition_schema:
+	schema_string = ', '.join(['%s %s' % (col, _TTypeId_to_PrimitiveType[ty])
+		for (col, ty) in partition_schema])
+	query += " PARTITIONED BY (%s)" % schema_string
+    if file_format == 'PARQUET':
+	query += " STORED AS PARQUET"
+    elif file_format == 'TEXTFILE':
+	query += ((" ROW FORMAT DELIMITED FIELDS TERMINATED BY '%s' "
+		   "LINES TERMINATED BY '%s' STORED AS TEXTFILE") %
+		   (field_terminator, line_terminator))
+    query += " LOCATION '%s'" % path
+    cursor.execute(query)
 
-    Uses `CREATE EXTERNAL TABLE`. Should be CSV or Avro/Parquet
-    """
-    raise NotImplementedError
-
-def from_pandas(cursor, df):
+def from_pandas(cursor, df, table, path, method='in_query',
+	file_format='TEXTFILE', field_terminator='\\t', line_terminator='\\n',
+	hdfs_host=None, webhdfs_port=50070, hdfs_user=None):
     """Create a BDF by shipping an in-memory pandas `DataFrame` into Impala
 
     TBD: could possible translate into a `VALUES()` statement instead of writing
     the data to HDFS first.
     """
-    raise NotImplementedError
+    table_name = _to_TableName(table)
+    columns = list(df.columns)
+    types = [_numpy_dtype_to_impala_PrimitiveType(ty) for ty in df.dtypes]
+    schema = zip(columns, types)
+    if method == 'in_query':
+	query = "INSERT INTO %s VALUES %s" % ', '.join([str(tuple(row))
+		for row in df.values])
+	cursor.execute(query)
+	register_external_table(cursor, table_name, schema, path,
+		file_format=file_format, field_terminator=field_terminator,
+		line_terminator=line_terminator)
+    elif method == 'webhdfs':
+	if file_format != 'TEXTFILE':
+	    raise ValueError("only TEXTFILE format supported for webhdfs")
+	from pywebdfs.webhdfs import PyWebHdfsClient
+	hdfs_client = PyWebHdfsClient(host=hdfs_host, port=webhdfs_port,
+		user=hdfs_user)
+	raw_data = StringIO()
+	df.to_csv(raw_data, sep=field_terminator,
+		line_terminator=line_terminator, header=False, index=False)
+	hdfs_client.create_file(path.lstrip('/'), raw_data.getvalue(), overwrite=overwrite)
+	raw_data.close()
+	register_external_table(cursor, table_name, schema, path,
+		file_format=file_format, field_terminator=field_terminator,
+		line_terminator=line_terminator)
+    else:
+	raise ValueError("method must be 'in_query' or 'webhdfs'; got %s" % method)
+    return read_sql_table(cursor, table_name.to_sql())
 
 
 class BigDataFrame(object):
 
-    def __init__(self, ast=None):
+    def __init__(self, ast):
 	self._query_ast = ast
 
     def fetch(self, cursor):
@@ -97,14 +182,16 @@ class BigDataFrame(object):
 	"""
 	raise NotImplementedError
 	# TODO: finish this
-	sql = 'CREATE TABLE %s AS %s STORED AS %s ...' % (name, self._query_ast.to_sql())
+	sql = 'CREATE TABLE %s AS %s STORED AS %s ...' % (name,
+		self._query_ast.to_sql())
 	cursor.execute(sql)
 	return read_sql_table(cursor, name)
 
-    def save_view(self, cursor, view_name, db_name=None):
+    def save_view(self, cursor, view):
 	"""Create a named view representing this BDF for later reference"""
-	table_name = TableName(view_name, db_name)
-	sql = 'CREATE VIEW %s AS %s' % (table_name.to_sql(), self._query_ast.to_sql())
+	table_name = _to_TableName(view)
+	sql = 'CREATE VIEW %s AS %s' % (table_name.to_sql(),
+		self._query_ast.to_sql())
 	cursor.execute(sql)
 	return read_sql_table(cursor, table_name.to_sql())
 
@@ -114,7 +201,18 @@ class BigDataFrame(object):
 	Distributed and no notion of order, so not guaranteed to be
 	reproducible.
 	"""
-	bdf = BigDataFrame(self._query_ast.limit(Literal(n)))
+	ast = copy(self._query_ast)
+	# compute the new LIMIT expression
+	if ast._limit:
+	    offset_expr = ast._limit._offset_expr
+	    orig_limit_string = ast._limit._limit_expr.to_sql()
+	    new_limit_string = ("CASE WHEN (%s) < %i THEN (%s) ELSE %i END" %
+		    (orig, n, orig, n))
+	    limit_elt = LimitElement(Literal(new_limit_string), offset_expr)
+	else:
+	    limit_elt = LimitElement(Literal(n), None)
+	ast._limit = limit_elt
+	bdf = BigDataFrame(ast)
 	return as_pandas(bdf.fetch(cursor))
 
     # for emulation of Pandas API
@@ -133,12 +231,19 @@ class BigDataFrame(object):
 	will be interpreted as a projection.
 	"""
 	if isinstance(obj, tuple) and len(obj) == 2:
-	    return self._getitem_tuple(obj)
+	    alias = _random_id('inline_', 4)
+	    table_ref = InlineView(self._query_ast.to_sql(), alias)
+	    (limit_elt, where) = self._getitem_filter(obj[0])
+	    select_list = self._getitem_projection(obj[1])
+	    return BigDataFrame(SelectStmt(select_list, table_ref, where=where, limit=limit_elt))
 	elif isinstance(obj, list):
-	    return self._getitem_projection(obj)
+	    alias = _random_id('inline_', 4)
+	    table_ref = InlineView(self._query_ast.to_sql(), alias)
+	    select_list = self._getitem_projection(obj)
+	    return BigDataFrame(SelectStmt(select_list, table_ref))
 	else:
 	    # single object, possibly a slice; wrap in list and get projection
-	    return self._getitem_projection([obj])
+	    return self[[obj]]
 
     def _getitem_tuple(self, obj):
 	raise NotImplementedError
@@ -206,8 +311,8 @@ class BigDataFrame(object):
 	if isinstance(obj, slice):
 	    if obj.step != 1 and obj.step is not None:
 		raise ValueError("slices can only have a step size of 1")
-	    if not isinstance(obj.start, (int, long)) or
-		    not isinstance(obj.stop, (int, long)):
+	    if (not isinstance(obj.start, (int, long)) or
+		    not isinstance(obj.stop, (int, long))):
 		raise ValueError("slice stop and start must be int/long")
 	    return (LimitElement(obj.stop - obj.start, obj.start), None)
 	raise ValueError("row indexer must be int/long/slice/Expr")
@@ -404,14 +509,32 @@ class SelectStmt(SQLNodeMixin, RelationalMixin):
 	# self._has_groupby = False
 	# self._has_agg = False
 
-    def projection(self, select_list):
-	table_ref = InlineView(self.to_sql(), _random_id())
-	return SelectStmt(select_list, table_ref)
+    # def projection(self, select_list):
+    #     table_ref = InlineView(self.to_sql(), _random_id())
+    #     return SelectStmt(select_list, table_ref)
 
-    def limit(self, n, offset=None):
-	select_list = [SelectItem()] # SELECT *
-	table_ref = InlineView(self.to_sql(), _random_id())
-	return SelectStmt(select_list, table_ref, limit=LimitElement(n, offset))
+    # def limit(self, n, offset=None):
+    #     # TODO: should this wrap the current query in an InlineView and then
+    #     # LIMIT?  Or should it just return a copy of the current query but with
+    #     # a modified LIMIT clause?
+
+    #     # this impl modifies.
+    #     ast = copy(self)
+    #     ast._limit = LimitElement(n, offset)
+    #     return ast
+
+    #     # this impl wraps.  But it breaks on queries like: SELECT * FROM (SELECT
+    #     # sepal_width, sepal_width FROM (SELECT sepal_length, sepal_width,
+    #     # petal_length, petal_width FROM (SELECT * FROM iris_text LIMIT 100) AS
+    #     # inline_IKDZ) AS inline_OMTW) AS RJZMILSQ LIMIT 5
+    #     # which was generated like this:
+    #     # query = 'SELECT * FROM iris_text LIMIT 100'
+    #     # bdf = impala.pandas.read_sql_query(cursor, query)
+    #     # bdf2 = bdf[['sepal_width','sepal_width']]
+    #     # bdf2.take(cursor, 5)
+    #     select_list = [SelectItem()] # SELECT *
+    #     table_ref = InlineView(self.to_sql(), _random_id())
+    #     return SelectStmt(select_list, table_ref, limit=LimitElement(n, offset))
 
     def to_sql(self):
 	sql = 'SELECT ' + ', '.join([s.to_sql() for s in self._select_list])
