8944b7c7dbcf652f5b93fb86fe706d2125db8511
Uri Laserson <laserson@cloudera.com>
2014-10-16 17:25:51 -0400

    Added missing import

--- impala/bdf.py
+++ impala/bdf.py
@@ -23,8 +23,8 @@ import pandas as pd
 
 from impala.util import (as_pandas, _random_id, _py_to_sql_string,
         _get_table_schema_hack)
-from impala._sql_model import (_to_TableName, BaseTableRef, SelectItem,
-        SelectStmt, Literal, InlineView, _create_table)
+from impala._sql_model import (_to_TableName, BaseTableRef, JoinTableRef,
+        SelectItem, SelectStmt, Literal, InlineView, TableName, _create_table)
 
 # utilities
 
