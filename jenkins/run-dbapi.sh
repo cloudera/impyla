f71e49696815fe1baeb7d5ae180e8c3c71ee34c6
Uri Laserson <laserson@cloudera.com>
2015-06-07 20:29:51 -0700

    [IMPYLA-52] Support kerberos for jenkins testing
    Fixes #52.

--- jenkins/run-dbapi.sh
+++ jenkins/run-dbapi.sh
@@ -18,14 +18,32 @@
 : ${IMPALA_PROTOCOL:?"IMPALA_PROTOCOL is unset"}
 : ${IMPALA_PORT:?"IMPALA_PORT is unset"}
 : ${PYTHON_VERSION:?"PYTHON_VERSION is unset"}
+: ${USE_KERBEROS:?"USE_KERBEROS is unset"}
+# the following are set by jenkins and are only needed if WORKSPACE not set
+#   GIT_URL
+#   GIT_BRANCH
 
-mkdir -p /tmp/impyla-dbapi/$BUILD_NUMBER
-TMP_DIR=$(mktemp -d -p /tmp/impyla-dbapi/$BUILD_NUMBER tmpXXXX)
+printenv
+
+mkdir -p /tmp/impyla-dbapi
+TMP_DIR=$(mktemp -d -p /tmp/impyla-dbapi tmpXXXX)
 
 function cleanup {
     rm -rf $TMP_DIR
 }
 trap cleanup EXIT
+
+# checkout impyla if necessary
+# this is necessary when run via SSH on a kerberized node
+if [ -z "$WORKSPACE" ]; then
+    : ${GIT_URL:?"GIT_URL is unset"}
+    : ${GIT_BRANCH:?"GIT_BRANCH is unset"}
+    cd $TMP_DIR
+    git clone $GIT_URL
+    cd impyla
+    git checkout origin/$GIT_BRANCH
+    WORKSPACE=$TMP_DIR/impyla
+fi
 
 # Build requested Python version
 cd $TMP_DIR
@@ -49,16 +67,25 @@ $PY_BIN_DIR/pip install virtualenv
 
 # Set up virtualenv and install prereqs
 cd $TMP_DIR
-VENV_NAME=impyla-dbapi-pyvenv-$BUILD_NUMBER
+VENV_NAME=impyla-dbapi-pyvenv
 $PY_BIN_DIR/virtualenv -p $PY_EXEC $VENV_NAME
 source $VENV_NAME/bin/activate
 pip install pytest
 if [ "$PY_MAJOR" -eq "2" -a "$PY_MINOR" -le "6" ]; then
     pip install unittest2  # for Python <= 2.6
 fi
+if [ $USE_KERBEROS = "True" ]; then
+    pip install git+https://github.com/laserson/python-sasl.git@cython
+fi
 
 # Build impyla
 cd $WORKSPACE && pip install .
+
+# Authenticate if using kerberos
+
+if [ $USE_KERBEROS = "True" ]; then
+    kinit -l 12h -kt /cdep/keytabs/systest.keytab systest
+fi
 
 # Run PEP 249 testing suite
 cd $TMP_DIR && py.test --pyargs impala.tests.test_dbapi_compliance
