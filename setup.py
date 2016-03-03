0c85ae3524498428ea6282b6f90cd4e8d7f08050
Wes McKinney <wesm@apache.org>
2016-03-03 14:51:46 -0800

    RLS: Bump version to 0.13.3

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ elif PY3:
 
 setup(
     name='impyla',
-    version='0.13.2',
+    version='0.13.3',
     description='Python client for the Impala distributed query engine',
     long_description=readme(),
     author='Uri Laserson',
