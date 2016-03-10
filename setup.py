5a73aa1c7e7903e16da56f3a3fee1ac0771ea20a
Wes McKinney <wesm@apache.org>
2016-03-09 16:18:01 -0800

    Bump version to 0.13.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ elif PY3:
 
 setup(
     name='impyla',
-    version='0.13.3',
+    version='0.13.4',
     description='Python client for the Impala distributed query engine',
     long_description=readme(),
     author='Uri Laserson',
