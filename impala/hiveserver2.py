0dee44c53a8eb0123123d227c86bc93546ddfa35
schaffino <joshuagbaxter@gmail.com>
2016-10-29 04:40:24 +0100

    Usability and speed improvements for fetching results (#223)
    * Allow convert_types cursor option to be used in normal fetch methods. Relating to issue #221i
    
        * Enable usage of convert_types outside of fetchcolumnar function.
        * Add fetchcbatch function so that you can work on columnar data without pulling the full result set.
    
    Additionally fix some commenting issues
    
    * pop(0) calls were a significant cause of slowdown in the materialisng rows from the cBatch object. These have been swapped for index based access.

--- impala/hiveserver2.py
+++ impala/hiveserver2.py
@@ -88,20 +88,25 @@ class HiveServer2Connection(Connection):
         convert_types : bool, optional
             When `False`, timestamps and decimal values will not be converted
             to Python `datetime` and `Decimal` values. (These conversions are
-            expensive.)
+            expensive.) Only applies when using HS2 protocol versions > 6.
+        dictify : bool, optional
+            When `True` cursor will return key value pairs instead of rows.
+        batch_cursor : bool, optional
+            When `True` cursor will return CBatches directly rather than rows. 
         fetch_error : bool, optional
             In versions of impala prior to 2.7.0, when an operation fails and 
             the impalad returns an error state, the error message is not always
             returned. In these cases the error message can be retrieved by a 
-            subsequent fetch call but this has the side effect of invalidating 
-            the query handle and  causing any further on the current query to 
-            functions like log or profile to cause an exception. 
+            subsequent fetch rpc call but this has a side effect of invalidating
+            the query handle and causing any further operations against it to 
+            fail. e.g. calling log() or profile(). 
+
             When set to `True` impyla will attempt to fetch the error message. 
             When set to `False`, this flag will cause impyla not to attempt to
-            fetch the message and the query handle. In this case the query 
+            fetch the message with a fetch call . In this case the query 
             handle remains valid and impyla will raise an exception with a 
-            message of "Operation is in ERROR_STATE". The Default option
-            is `True`.
+            message of "Operation is in ERROR_STATE". 
+            The Default option is `True`.
 
         Returns
         -------
@@ -335,8 +340,8 @@ class HiveServer2Cursor(Cursor):
 
     def _wait_to_finish(self):
         # Prior to IMPALA-1633 GetOperationStatus does not populate errorMessage
-        # in case of failure. If not populated queries that return results
-        # can get a failure description witha further call to FetchResults rpc.
+        # in case of failure. If not populated, queries that return results
+        # can get a failure description with a further call to FetchResults rpc.
         loop_start = time.time()
         while True:
             req = TGetOperationStatusReq(operationHandle=self._last_operation.handle)
@@ -414,6 +419,35 @@ class HiveServer2Cursor(Cursor):
         except StopIteration:
             return None
 
+
+    def fetchcbatch(self):
+        '''Return a CBatch object of any data currently in the buffer or
+           if no data currently in buffer then fetch a batch'''
+        if not self._last_operation.is_columnar:
+            raise NotSupportedError("Server does not support columnar "
+                                    "fetching")
+        if not self.has_result_set:
+            raise ProgrammingError(
+                "Trying to fetch results on an operation with no results.")
+        if len(self._buffer) > 0:
+            log.debug('fetchcbatch: buffer has data in. Returning it and wiping buffer')
+            batch = self._buffer
+            self._buffer = Batch()
+            return batch 
+        elif self._last_operation_active:
+            log.debug('fetchcbatch: buffer empty and op is active => fetching '
+                      'more data')
+            batch = (self._last_operation.fetch(
+                         self.description,
+                         self.buffersize,
+                         convert_types=self.convert_types))
+            if len(batch) == 0:
+               return None 
+            return batch
+        else:
+           return None 
+
+
     def fetchmany(self, size=None):
         # PEP 249
         self._wait_to_finish()
@@ -483,7 +517,8 @@ class HiveServer2Cursor(Cursor):
             log.debug('__next__: buffer empty and op is active => fetching '
                       'more data')
             self._buffer = self._last_operation.fetch(self.description,
-                                                      self.buffersize)
+                                                      self.buffersize,
+                                                      convert_types=self.convert_types)
             if len(self._buffer) == 0:
                 log.debug('__next__: no more data to fetch')
                 raise StopIteration
@@ -744,17 +779,21 @@ class Column(object):
         self.data_type = data_type
         self.values = values
         self.nulls = nulls
+        self.rows_left = len(self.values)
 
     def __len__(self):
-        return len(self.values)
+        return self.rows_left
 
     def __str__(self):
         return 'Column(type={0}, values={1}, nulls={2})'.format(
             self.data_type, self.values, self.nulls)
 
     def pop(self):
-        isnull = self.nulls.pop(0)
-        value = self.values.pop(0)
+        if self.rows_left < 1:
+            raise StopIteration 
+        isnull = self.nulls[-self.rows_left]
+        value = self.values[-self.rows_left]
+        self.rows_left -= 1
         return None if isnull else value
 
 
@@ -766,6 +805,7 @@ class CBatch(Batch):
                  for (i, col) in enumerate(trowset.columns)]
         num_cols = len(tcols)
         num_rows = len(tcols[0].values)
+        
         log.debug('CBatch: input TRowSet num_cols=%s num_rows=%s tcols=%s',
                   num_cols, num_rows, tcols)
 
