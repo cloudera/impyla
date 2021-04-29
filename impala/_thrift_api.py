2e39d0c231be0f439b1d571c916f3ff48ccd5323
Attila Jeges <attilaj@cloudera.com>
2021-04-29 12:30:32 +0200

    Remove references to 'sasl', always use 'pure-sasl' package

--- impala/_thrift_api.py
+++ impala/_thrift_api.py
@@ -465,24 +465,10 @@ def get_transport(socket, host, kerberos_service_name, auth_mechanism='NOSASL',
         auth_mechanism = 'PLAIN'  # sasl doesn't know mechanism LDAP
     # Initializes a sasl client
     from thrift_sasl import TSaslClientTransport
-    try:
-        import sasl  # pylint: disable=import-error
+    from impala.sasl_compat import PureSASLClient
 
-        def sasl_factory():
-            sasl_client = sasl.Client()
-            sasl_client.setAttr('host', host)
-            sasl_client.setAttr('service', kerberos_service_name)
-            if auth_mechanism.upper() in ['PLAIN', 'LDAP']:
-                sasl_client.setAttr('username', user)
-                sasl_client.setAttr('password', password)
-            sasl_client.init()
-            return sasl_client
-    except ImportError:
-        log.warn("Unable to import 'sasl'. Fallback to 'puresasl'.")
-        from impala.sasl_compat import PureSASLClient
-
-        def sasl_factory():
-            return PureSASLClient(host, username=user, password=password,
-                                  service=kerberos_service_name)
+    def sasl_factory():
+        return PureSASLClient(host, username=user, password=password,
+                              service=kerberos_service_name)
 
     return TSaslClientTransport(sasl_factory, auth_mechanism, socket)
