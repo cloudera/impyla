758b6c79d1e99b25e339ccb352c0d752094b0fed
Attila Jeges <attilaj@cloudera.com>
2021-05-26 11:55:46 +0200

    Upgrade to thrift-sasl 0.4.3

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     packages=find_packages(),
     install_package_data=True,
     package_data={'impala.thrift': ['*.thrift']},
-    install_requires=['six', 'bitarray', 'thrift==0.11.0', 'thrift_sasl==0.4.3a2'],
+    install_requires=['six', 'bitarray', 'thrift==0.11.0', 'thrift_sasl==0.4.3'],
     extras_require={
         "kerberos": ['kerberos>=1.3.0'],
     },
