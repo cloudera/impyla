746013bb9a1fbcb731e9d40b8bf425f8e882b7df
Csaba Ringhofer <csringhofer@cloudera.com>
2024-01-30 19:28:04 +0100

    Update versioneer to 0.29 (needed for Python 3.12)
    The old version used deprecated functions that were
    removed in Python 3.12.
    
    The change only contains code generated by:
    versioneer install

--- impala/__init__.py
+++ impala/__init__.py
@@ -14,6 +14,5 @@
 
 from __future__ import absolute_import
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
